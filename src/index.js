a9d244aa4f221ae967893df2a529787dc85c2ce9
Kelli Lilly <kelli.lilly05@gmail.com>
2022-12-15 11:58:25 -0600

    Using Manav's code to accurately display

--- src/index.js
+++ src/index.js
@@ -1,41 +1,33 @@
+// Initial const
 const root = document.querySelector("#root");
-const search = document.querySelector("input");
 
-const response = await fetch("https://jsonplaceholder.typicode.com/users");
+let letters = "";
 
-const people = await response.json();
-
-console.log(people);
-
+// Function declarations/expressions
 function createBioCard(user) {
   return `
-    <section class="bg-slate-900 text-white p-6 min-w-max rounded-md">
-    <h2 class="text-2xl font-semibold my-2">${user.name}</h2>
-    <ul class="flex flex-col gap-y-4 my-4">
-      <li><a href="mailto:${user.email}">${user.email}</a></li>
-      <li><a href="tel:${user.phone}">${user.phone}</a></li>
-      <li><a href="https://www.google.com/maps/place/${user.address.geo.lat},${user.address.geo.lng}">${user.address.street}, ${user.address.city}</a></li>
-    </ul>
-
-      <q class="italic">Centralized empowering task-force</q>
-  </section>
-    `;
-}
-function filterByName(searchLetters, listOfUsers) {
-  return listOfUsers.filter((person) => person.name.includes(searchLetters));
+  <section class="bg-slate-900 text-white p-6 min-w-max rounded-md">
+  <h2 class="text-2xl font-semibold my-2">${user.name}</h2>
+  <ul class="flex flex-col gap-y-4 my-4">
+    <li><a href="mailto:${user.email}">${user.email}</a></li>
+    <li><a href="tel:${user.phone}">${user.phone}</a></li>
+    <li><a href="https://www.google.com/maps/place/${user.address.geo.lat},${user.address.geo.lng}">${user.address.street}, ${user.address.city}</a></li>
+  </ul>
+    <q class="italic">Centralized empowering task-force</q>
+</section>
+  `;
 }
 
-root.classList.add(
-  "container",
-  "mx-auto",
-  "items-center",
-  "grid",
-  "grid-cols-3",
-  "gap-4"
-);
+function filterByName(listOfUsers, searchLetters) {
+  return listOfUsers.filter((user) => user.name.includes(searchLetters));
+}
 
-const bioCards = people.map(createBioCard).join(" ");
-let letters = "";
+// Business logic
+const resp = await fetch("https://jsonplaceholder.typicode.com/users");
+const users = await resp.json();
+const bioCardsHTML = users.map(createBioCard).join("");
+
+root.classList.add("flex", "flex-col", "gap-y-8", "items-center");
 
 root.innerHTML = `
 <div>
@@ -43,13 +35,16 @@ root.innerHTML = `
 <input type="search" id="search" placeholder="🔍" />
 </div>
 <main class="items-center grid grid-cols-3 gap-4">
-  ${bioCards}
+  ${bioCardsHTML}
 </main
 `;
 
+// ⚠️ 'input' doesn't exist until we update the 'root' 'innerHTML'
+const search = document.querySelector("input");
+
 search.addEventListener("keydown", (event) => {
   letters += event.key;
-  const filteredUsers = filterByName(people, letters);
+  const filteredUsers = filterByName(users, letters);
 
   console.log(filteredUsers);
 });
