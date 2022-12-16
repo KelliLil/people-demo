c25f7590c7882de4c1773f66c2783c14e30760dd
Kelli Lilly <kelli.lilly05@gmail.com>
2022-12-16 11:48:38 -0600

    refactor: :building_construction: utils

--- src/index.js
+++ src/index.js
@@ -1,29 +1,7 @@
-// Initial const
+import { createBioCard, filterByName } from "./utils";
+
 const root = document.querySelector("#root");
 
-// This keeps track of what we type in the search bar
-const letters = "";
-
-// Function declarations/expressions
-function createBioCard(user) {
-  return `
-  <section class="bg-slate-900 text-white p-6 min-w-max rounded-md">
-  <h2 class="text-2xl font-semibold my-2">${user.name}</h2>
-  <ul class="flex flex-col gap-y-4 my-4">
-    <li><a href="mailto:${user.email}">${user.email}</a></li>
-    <li><a href="tel:${user.phone}">${user.phone}</a></li>
-    <li><a href="https://www.google.com/maps/place/${user.address.geo.lat},${user.address.geo.lng}">${user.address.street}, ${user.address.city}</a></li>
-  </ul>
-    <q class="italic">Centralized empowering task-force</q>
-</section>
-  `;
-}
-
-function filterByName(listOfUsers, searchLetters) {
-  return listOfUsers.filter((user) => user.name.includes(searchLetters));
-}
-
-// Render the cards
 function renderCards(currentUsers) {
   main.innerHTML = currentUsers.map(createBioCard).join("");
 }
@@ -31,7 +9,6 @@ function renderCards(currentUsers) {
 // Business logic
 const resp = await fetch("https://jsonplaceholder.typicode.com/users");
 const users = await resp.json();
-// Take array of users and 1-1 for each output the HTML string, Join array back into string
 const bioCardsHTML = users.map(createBioCard).join("");
 
 root.classList.add("flex", "flex-col", "gap-y-8", "items-center");
@@ -48,13 +25,12 @@ root.innerHTML = `
 
 const main = document.querySelector("main");
 
-// ⚠️ 'input' doesn't exist until we update the 'root' 'innerHTML'
 const search = document.querySelector("input");
 
 search.addEventListener(
+  // Fires whenever value of input changes
   "input",
 
-  // Browser API models the event as an event object
   (event) => {
     const filteredUsers = filterByName(users, event.target.value);
     renderCards(filteredUsers);
